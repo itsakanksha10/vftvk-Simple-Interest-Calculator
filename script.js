29d017365b608b98900e28210b9cc1a3c5111655
Akanksha Shrivastav <itsakanksha10@gmail.com>
2021-07-30 22:51:33 +0530

    Modified as per requirements

--- script.js
+++ script.js
@@ -1,13 +1,18 @@
 function compute(){
     p = document.getElementById("principal").value;
-    r = document.getElementById("interest").value;
-    t = document.getElementById("year").value;
-    var interest = (p*r*t)/100;
-    
-    document.getElementById("deposit").innerHTML = p;
-    document.getElementById("rate").innerHTML = r.toString() + '%';
-    document.getElementById("amount").innerHTML = parseFloat(p);
-    document.getElementById("end-year").innerHTML = parseInt(2021) + parseInt(t);
+    if (p > 0){
+        r = document.getElementById("interest").value;
+        t = document.getElementById("year").value;
+        var interest = (p*r*t)/100;
+        
+        document.getElementById("deposit").innerHTML = p;
+        document.getElementById("rate").innerHTML = r.toString() + '%';
+        document.getElementById("amount").innerHTML = parseFloat(p);
+        document.getElementById("end-year").innerHTML = parseInt(2021) + parseInt(t);
+    } else {
+        alert('Please enter a positive number for amount');
+        document.getElementById("principal").focus();
+    }
 }
 
 function sliderChange(){
