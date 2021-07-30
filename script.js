659b3632bd2fd3b5f1679da2e2de3b5b392beafb
Akanksha Shrivastav <itsakanksha10@gmail.com>
2021-07-30 22:18:01 +0530

    Completed

--- script.js
+++ script.js
@@ -1,6 +1,16 @@
-function compute()
-{
+function compute(){
     p = document.getElementById("principal").value;
+    r = document.getElementById("interest").value;
+    t = document.getElementById("year").value;
+    var interest = (p*r*t)/100;
     
+    document.getElementById("deposit").innerHTML = p;
+    document.getElementById("rate").innerHTML = r.toString() + '%';
+    document.getElementById("amount").innerHTML = parseFloat(p) + parseFloat(interest);
+    document.getElementById("end-year").innerHTML = parseInt(2021) + parseInt(t);
 }
-        
+
+function sliderChange(){
+    var value = document.getElementById('interest').value;
+    document.getElementById('interest-display').innerHTML = value.toString() + '%';
+}
